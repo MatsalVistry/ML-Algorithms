dae1f3fcd2fefde1c6f072e50de29abe52d90413
Vatsal Mistry <vatsalmistry4796@gmail.com>
2020-06-11 16:33:39 -0500

    completed simple linear regression

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Python/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Python/simple_linear_regression.py
@@ -1,39 +1,33 @@
-# Simple Linear Regression
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-# Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Training the Simple Linear Regression model on the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+lr = LinearRegression()
+lr.fit(X_train,y_train)
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
+y_predicted = lr.predict(X_test)
 
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
+plt.scatter(X_train,y_train, color = 'red')
+plt.plot(X_train, lr.predict(X_train),color = 'blue')
+plt.title('Salary vs Experience(Training)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
+plt.scatter(X_test,y_test, color = 'red')
+plt.plot(X_test, y_predicted,color = 'blue') 
+#plot all our x test values and the y values we predicted for them and draw a line through it
+plt.title('Salary vs Experience(Test)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
+print(lr.predict([[4],[2]]))
