231e033a474256924d8b80a91f5b1a12dbbba982
Vatsal Mistry <vatsalmistry4796@gmail.com>
2020-06-17 15:09:23 -0500

    working decision tree regression

--- Part 2 - Regression/Section 8 - Decision Tree Regression/Python/decision_tree_regression.py
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/Python/decision_tree_regression.py
@@ -1,29 +1,22 @@
-# Decision Tree Regression
-
-# Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
-
-# Training the Decision Tree Regression model on the whole dataset
+import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
-regressor.fit(X, y)
 
-# Predicting a new result
-regressor.predict([[6.5]])
+dataset = pd.read_csv('Position_Salaries.csv')
 
-# Visualising the Decision Tree Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
-plt.xlabel('Position level')
+X = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+
+dtr = DecisionTreeRegressor(random_state=0)
+dtr.fit(X,y)
+
+# inneficient for decision tree regression
+Xgrid = np.arrange(min(X),max(X),.1)
+Xgrid = Xgrid.reshape((len(Xgrid),1))
+plt.scatter(X,y, color = 'red')
+plt.plot(Xgrid,dtr.predict(Xgrid), color='blue')
+plt.title('Decision Tree Regression')
+plt.xlabel('Position')
 plt.ylabel('Salary')
-plt.show()
+print(dtr.predict([[6.5]]))
