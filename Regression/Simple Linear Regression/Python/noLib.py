3281ec04ca9777fdafe721dd1e98724700db52c0
Vatsal Mistry <vatsalmistry4796@gmail.com>
2020-06-26 12:56:59 -0500

    working linear regression no library

---
+++ Regression/Simple Linear Regression/Python/noLib.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def splitData(X, y, testSize):
+    splitLoc = math.floor(len(dataset)*(1-testSize))
+
+    xtrain = X[:splitLoc,:]
+    ones = np.ones((len(xtrain),1))
+    xtrain = np.concatenate((xtrain, ones),1)
+
+    xtest = X[splitLoc:,:]
+    ones = np.ones((len(xtest),1))
+    xtest = np.concatenate((xtest, ones),1)
+
+    ytrain = y[:splitLoc,:]
+    ytest = y[splitLoc:,:]
+    return xtrain,xtest,ytrain,ytest
+
+def costFunction(X,y,theta):
+    inner = np.power((np.matmul(X, theta) - y), 2)
+    return np.sum(inner) / (2 * len(X))  
+
+def gradientDescent(X, y, theta, alpha, iters):
+    for i in range(iters):
+        theta = theta - (alpha/len(X)) * np.sum(np.multiply((np.matmul(X, theta) - y), X[:,0].reshape(-1,1)))
+        cost = costFunction(X, y, theta)
+        if i % 10 == 0:
+            print(cost)
+    return theta, cost
+
+def featureScale(X):
+    for i in range(len(X[0])-1):
+        X[:, i] = (X[:,i] - X[:,i].mean())/X[:,i].std()
+    return X
+
+def rsquared(X,y,theta):
+    predicted = X @ theta
+    sstot = np.sum(pow(y-y.mean(),2))
+    ssres = np.sum(pow(y-predicted,2))
+    return 1-(ssres/sstot)
+
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values.reshape(-1,1)
+ones = np.ones((len(X),1))
+X = np.concatenate((X, ones),1)
+# xtrain,xtest,ytrain,ytest = splitData(X,y, .2)
+# featureScale(X)
+# xtest = featureScale(xtest)
+
+alpha = .001
+iterations = 5000
+theta = np.array([[.5,.5]]).T
+theta, cost = gradientDescent(X,y,theta,alpha,iterations)
+print(theta)
+
+plt.scatter(X[:,0],y)
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.title('Salary vs Years')
+predicted = X @ theta
+plt.plot(X[:,0],predicted)
+
+print("Score: "+ str(rsquared(X,y,theta)))
